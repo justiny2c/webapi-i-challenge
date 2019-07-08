297d8d2209a1b5b751e641e481985863ce21a87d
justiny2c <justiny2c@gmail.com>
2019-07-08 12:46:23 -0700

    Initial push

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+
+const dbModel = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+const port = 6000;
+
+server.listen(port, () => console.log(`Running on port ${port}`))
